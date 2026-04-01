c30cdd06377c9329cc784794524991a0d2cb9641
Pradheepa Jaya Shree T <pradheepajayashree@gmail.com>
2026-04-01 14:50:57 +0530

    Create use_case_1.java

---
+++ use_case_1.java
@@ -0,0 +1,8 @@
+public class use_case_1 {
+     public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+        System.out.println("Application Name: Book My Stay App");
+        System.out.println("Version: 1.0");
+    }
+}
